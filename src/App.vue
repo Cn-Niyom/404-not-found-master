4ac9887eb7e072f819cc524bad95b683aad508e5
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-06-19 19:35:26 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="min-h-screen overflow-hidden">
+    <div class="xl:mx-[77px] mt-[20px] xl:mt-[40px] mx-11  ">
+      <p class="text-2xl font-bold text-[#333333]">404 NOT FOUND</p>
+      <div class="xl:mt-[200px] flex flex-col xl:flex-row ">
+        <img src="./assets/Scarecrow.png" alt="Scarecrow" class="xl:w-[539.22px] w-[286.83px] my-[64px] xl:my-0">
+        <div class="xl:ml-[118.78px]">
+          <p
+            class="w-[350px] xl:w-[586px] h-[142px] xl:h-[190px] text-[#333333] text-[48px]  xl:text-[64px] font-bold leading-[-3.5%]">
+            I
+            have bad
+            news for you
+          </p>
+          <p class="mt-[30px] xl:mt-[36px] xl:w-[381px] xl:h-[108px] text-lg xl:text-2xl text-[#4F4F4F] leading-[-3.5%]">
+            The
+            page you
+            are looking
+            for might be
+            removed or is
+            temporarily
+            unavailable</p>
+          <button class="bg-[#333333] text-white px-[43px] py-[23px] w-[216px] h-[68px] mt-[88px]">
+            <p class="text-sm ">Back to homepage</p>
+          </button>
+        </div>
+      </div>
+    </div>
+    <p class="text-center bottom-[24px] absolute w-full text-[#BDBDBD] font-bold ">create by Cn-Niyom - devChallenges.io
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
